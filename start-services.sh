a1927dea13000c83fc262e02df0d59648dcef39a
CosminIcatoiu <cosmin.icatoiu@gmail.com>
2019-03-25 15:50:30 +0200

    Removed sudo from script

--- start-services.sh
+++ start-services.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-#Build all the components
-docker-compose build
-
 #Start the service as a deamon
 docker-compose run -d --name tema_service_run_1 service
 
 #Get the ip of the service in the created network
-ip_s=$(sudo docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' tema_service_run_1)
+ip_s=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' tema_service_run_1)
 
-#Start the client
-docker-compose run -T -e ip_service=$ip_s client
+#Start the clientdocker-compose run -T -e ip_service=$ip_s client
 
 
